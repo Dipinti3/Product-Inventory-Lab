90251f113b5f06811e5905981f9b10b07ce1df8b
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-03 15:42:49 -0400

    added properties in Sneaker Class

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Sneaker {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private float size;
+    private int quantity;
+    private float price;
+    
 }
