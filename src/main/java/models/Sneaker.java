1c9e8e0a4ad50fea20d73fcc52269eb3e7e36373
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-03 17:20:13 -0400

    added parameterized constructors to sneaker

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -9,7 +9,45 @@ public class Sneaker {
     private int quantity;
     private float price;
 
-   public void setName(String name) {
+    public Sneaker(){}
+
+    public Sneaker(int id, String name, String brand, String sport, int quantity,
+                   float price) {
+        this.id=id;
+        this.name=name;
+        this.brand=brand;
+        this.sport=sport;
+        this.quantity=quantity;
+        this.price=price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+
+
+    public void setName(String name) {
         this.name=name;
     }
 
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -16,5 +16,28 @@ public class SneakerTest {
         Assert.assertEquals(expected,testSneaker.getName());
     }
 
+    @Test
+    public void testParameterizedConstructor(){
+        //given
+
+        //when
+        int expectedId=6;
+
+        String expectedName="Stan Smith";
+        String expectedBrand="Adidas";
+        String expectedSport="Tennis";
+        int expectedQuantity=10;
+        float expectedPrice=80.00f;
+        Sneaker sweetAdidas = new Sneaker(expectedId,expectedName, expectedBrand,
+                expectedSport, expectedQuantity, expectedPrice);
+        //then
+        Assert.assertEquals(expectedId,sweetAdidas.getId());
+        Assert.assertEquals(expectedName,sweetAdidas.getName());
+        Assert.assertEquals(expectedBrand,sweetAdidas.getBrand());
+        Assert.assertEquals(expectedSport,sweetAdidas.getSport());
+        Assert.assertEquals(expectedQuantity,sweetAdidas.getQuantity());
+        Assert.assertEquals(expectedPrice,sweetAdidas.getPrice(),0.0);
+    }
+
 
 }
